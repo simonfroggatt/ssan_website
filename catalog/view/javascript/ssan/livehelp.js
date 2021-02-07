aafc5249cb0196f4efcca1419875d156ac1b3453
simonfroggatt <simon@safetysignsandnotices.co.uk>
2021-02-07 09:50:05 +0400

    JQuery 3.5.1

--- catalog/view/javascript/ssan/livehelp.js
+++ catalog/view/javascript/ssan/livehelp.js
@@ -5,7 +5,7 @@
     // JavaScript
     Chatstack.e = []; Chatstack.ready = function (c) { Chatstack.e.push(c); }
     var b = d.createElement('script'); b.type = 'text/javascript'; b.async = true;
-    b.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + Chatstack.server + '/livehelp/scripts/jquery.livehelp.min.js';
+    b.src = ('https:' == d.location.protocol ? 'https://' : 'https://') + Chatstack.server + '/livehelp/scripts/jquery.livehelp.min.js';
     var s = d.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(b, s);
   })(document);
