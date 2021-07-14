4be8fe9cd33df9116fbd9c70b3097cb48407bb66
simonfroggatt <simonfroggatt76@gmail.com>
2021-07-14 14:47:45 +0400

    livehelp issues

--- catalog/view/javascript/ssan/livehelp.js
+++ catalog/view/javascript/ssan/livehelp.js
@@ -1,4 +1,4 @@
-
+/*
 
   var Chatstack = { server: 'www.safetysignsandnotices.co.uk' };
   (function(d, undefined) {
@@ -12,3 +12,15 @@
 
   Chatstack.colors = {tab: {normal: '#999999', hover: '#c9cacc'}};
   Chatstack.theme = 'dark';
+*/
+
+
+  var Chatstack = { server: 'www.safetysignsandnotices.co.uk' };
+  (function(d, undefined) {
+  // JavaScript
+  Chatstack.e = []; Chatstack.ready = function (c) { Chatstack.e.push(c); }
+  var b = d.createElement('script'); b.type = 'text/javascript'; b.async = true;
+  b.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + Chatstack.server + '/livehelp/scripts/js.min.js';
+  var s = d.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(b, s);
+})(document);
